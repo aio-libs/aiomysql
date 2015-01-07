f36fd362d02bf191ca65f9e62ac5b14b873b7ad6
Nickolai Novik <is.infinity@yahoo.com>
2015-01-07 22:38:46 +0200

    fix example test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,27 +1,19 @@
 import aiomysql
-from tests import base
+from tests._testutils import run_until_complete
+from tests.base import AIOPyMySQLTestCase
 
-class TestExample(base.AIOPyMySQLTestCase):
+class TestExample(AIOPyMySQLTestCase):
 
+    @run_until_complete
     def test_example(self):
-        conn = yield from aiomysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='mysql')
-
+        conn = yield from aiomysql.connect(host='127.0.0.1', port=3306,
+                                           user='root', passwd='', db='mysql',
+                                           loop=self.loop)
 
         cur = conn.cursor()
-
-        cur.execute("SELECT Host,User FROM user")
-
-        # print cur.description
-
-        # r = cur.fetchall()
-        # print r
-        # ...or...
-        u = False
-
-        for r in cur.fetchall():
-            u = u or conn.user in r
-
-        self.assertTrue(u)
-
+        yield from cur.execute("SELECT Host,User FROM user")
+        print(cur.description)
+        r = cur.fetchall()
+        print(r)
         cur.close()
         conn.close()
