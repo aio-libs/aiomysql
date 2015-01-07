18c132516ceca76807716de35eeac7096024fe3e
Nickolai Novik <is.infinity@yahoo.com>
2015-01-07 22:30:47 +0200

    fix nextset

--- tests/test_nextset.py
+++ tests/test_nextset.py
@@ -1,68 +1,67 @@
 import unittest
 
-from tornado.testing import gen_test
-
 from tests import base
+from tests._testutils import run_until_complete
 
 
-class TestNextset(base.PyMySQLTestCase):
+class TestNextset(base.AIOPyMySQLTestCase):
 
     def setUp(self):
         super(TestNextset, self).setUp()
         self.con = self.connections[0]
 
-    @gen_test
+    @run_until_complete
     def test_nextset(self):
         cur = self.con.cursor()
-        yield cur.execute("SELECT 1; SELECT 2;")
+        yield from cur.execute("SELECT 1; SELECT 2;")
         self.assertEqual([(1,)], list(cur))
 
-        r = yield cur.nextset()
+        r = yield from cur.nextset()
         self.assertTrue(r)
 
         self.assertEqual([(2,)], list(cur))
-        res = yield cur.nextset()
+        res = yield from cur.nextset()
         self.assertIsNone(res)
 
-    @gen_test
+    @run_until_complete
     def test_skip_nextset(self):
         cur = self.con.cursor()
-        yield cur.execute("SELECT 1; SELECT 2;")
+        yield from cur.execute("SELECT 1; SELECT 2;")
         self.assertEqual([(1,)], list(cur))
 
-        yield cur.execute("SELECT 42")
+        yield from cur.execute("SELECT 42")
         self.assertEqual([(42,)], list(cur))
 
-    @gen_test
+    @run_until_complete
     def test_ok_and_next(self):
         cur = self.con.cursor()
-        yield cur.execute("SELECT 1; commit; SELECT 2;")
+        yield from cur.execute("SELECT 1; commit; SELECT 2;")
         self.assertEqual([(1,)], list(cur))
-        res = yield cur.nextset()
+        res = yield from cur.nextset()
         self.assertTrue(res)
-        res = yield cur.nextset()
+        res = yield from cur.nextset()
         self.assertTrue(res)
         self.assertEqual([(2,)], list(cur))
-        res = yield cur.nextset()
+        res = yield from cur.nextset()
         self.assertIsNone(res)
 
     @unittest.expectedFailure
-    @gen_test
+    @run_until_complete
     def test_multi_cursor(self):
         cur1 = self.con.cursor()
         cur2 = self.con.cursor()
 
-        yield cur1.execute("SELECT 1; SELECT 2;")
-        yield cur2.execute("SELECT 42")
+        yield from cur1.execute("SELECT 1; SELECT 2;")
+        yield from cur2.execute("SELECT 42")
 
         self.assertEqual([(1,)], list(cur1))
         self.assertEqual([(42,)], list(cur2))
 
-        res = yield cur1.nextset()
+        res = yield from cur1.nextset()
         self.assertTrue(res)
 
         self.assertEqual([(2,)], list(cur1))
-        res = yield cur.nextset()
+        res = yield from cur1.nextset()
         self.assertIsNone(res)
 
     #TODO: How about SSCursor and nextset?
