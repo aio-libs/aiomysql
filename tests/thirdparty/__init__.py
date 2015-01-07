7f536b248fe45b783b237543cd3ac7252c7eed40
Nickolai Novik <is.infinity@yahoo.com>
2015-01-08 00:16:30 +0200

    remove unused sourses

--- tests/thirdparty/__init__.py
+++
@@ -1,5 +0,0 @@
-from .tests.thirdparty.test_MySQLdb import *
-
-if __name__ == "__main__":
-    import unittest
-    unittest.main()
--- tests/thirdparty/test_MySQLdb/__init__.py
+++
@@ -1,5 +0,0 @@
-from .tests.thirdparty.test_MySQLdb.test_MySQLdb_nonstandard import *
-
-if __name__ == "__main__":
-    import unittest
-    unittest.main()
