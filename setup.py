43e37317181e91fa7e79fd6e358e20634b7d2207
Nickolai Novik <is.infinity@yahoo.com>
2015-01-07 22:09:36 +0200

    upd setup

--- setup.py
+++ setup.py
@@ -1,34 +1,59 @@
-#!/usr/bin/env python
+import os
+import re
+import sys
 from setuptools import setup, find_packages
 
-try:
-    with open('README.rst') as f:
-        readme = f.read()
-except IOError:
-    readme = ''
 
-setup(
-    name="Tornado-MySQL",
-    version="0.2",
-    url='https://github.com/PyMySQL/Tornado-MySQL',
-    author='INADA Naoki',
-    author_email='songofacandy@gmail.com',
-    description='Pure Python MySQL Driver for Tornado',
-    install_requires=['tornado>=4.0'],
-    long_description=readme,
-    license="MIT",
-    packages=find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Database',
-    ],
-)
+install_requires = []
+
+PY_VER = sys.version_info
+
+if PY_VER >= (3, 4):
+    pass
+elif PY_VER >= (3, 3):
+    install_requires.append('asyncio')
+else:
+    raise RuntimeError("aiomysql doesn't suppport Python earllier than 3.3")
+
+
+def read(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__),
+                           'aiomysql', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            raise RuntimeError('Cannot find version in aiomysql/__init__.py')
+
+classifiers=[
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Database',
+],
+
+
+setup(name='aiomysql',
+      version=read_version(),
+      description=('MySQL driver for asyncio.'),
+      long_description='\n\n'.join((read('README.rst'), read('CHANGES.rst'))),
+      classifiers=classifiers,
+      platforms=['POSIX'],
+      author='',
+      author_email='',
+      url='http://aiomysql.readthedocs.org',
+      download_url='https://pypi.python.org/pypi/aiomysql',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=install_requires,
+      include_package_data = True)
