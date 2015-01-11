59942415b9931099b8cfc13c2bfcfe6433253628
Nickolai Novik <is.infinity@yahoo.com>
2015-01-12 00:09:45 +0200

    separate test cases, create new file with bulk insert tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,3 @@
-import asyncio
 import time
 import datetime
 
@@ -6,7 +5,6 @@ from tests import base
 from pymysql import util
 from pymysql.err import ProgrammingError
 
-import aiomysql.cursors
 from tests._testutils import run_until_complete
 
 
@@ -202,92 +200,3 @@ class TestConversion(base.AIOPyMySQLTestCase):
         # make sure that transaction flag is down
         transaction_flag = conn.get_transaction_status()
         self.assertFalse(transaction_flag)
-
-
-
-class TestBulkInserts(base.AIOPyMySQLTestCase):
-    cursor_type = aiomysql.cursors.DictCursor
-
-    def setUp(self):
-        super(TestBulkInserts, self).setUp()
-        self.conn = conn = self.connections[0]
-        c = conn.cursor(self.cursor_type)
-
-        @asyncio.coroutine
-        def prepare():
-            # create a table ane some data to query
-            yield from c.execute("drop table if exists bulkinsert;")
-            yield from c.execute(
-                """CREATE TABLE bulkinsert
-                (
-                id int(11),
-                name char(20),
-                age int,
-                height int,
-                PRIMARY KEY (id)
-                )
-                """)
-
-        self.loop.run_until_complete(prepare())
-
-    @asyncio.coroutine
-    def _verify_records(self, data):
-        conn = self.connections[0]
-        cursor = conn.cursor()
-        yield from cursor.execute(
-            "SELECT id, name, age, height from bulkinsert")
-        result = cursor.fetchall()
-        yield from cursor.execute('commit')
-        self.assertEqual(sorted(data), sorted(result))
-
-    @run_until_complete
-    def test_bulk_insert(self):
-        conn = self.connections[0]
-        cursor = conn.cursor()
-
-        data = [(0, "bob", 21, 123), (1, "jim", 56, 45), (2, "fred", 100, 180)]
-        yield from cursor.executemany(
-            "insert into bulkinsert (id, name, age, height) "
-            "values (%s,%s,%s,%s)", data)
-        self.assertEqual(
-            cursor._last_executed, bytearray(
-                b"insert into bulkinsert (id, name, age, height) values "
-                b"(0,'bob',21,123),(1,'jim',56,45),(2,'fred',100,180)"))
-        yield from cursor.execute('commit')
-        yield from self._verify_records(data)
-
-    @run_until_complete
-    def test_bulk_insert_multiline_statement(self):
-        conn = self.connections[0]
-        cursor = conn.cursor()
-        data = [(0, "bob", 21, 123), (1, "jim", 56, 45), (2, "fred", 100, 180)]
-        yield from cursor.executemany("""insert
-            into bulkinsert (id, name,
-            age, height)
-            values (%s,
-            %s , %s,
-            %s )
-             """, data)
-        self.assertEqual(cursor._last_executed, bytearray(b"""insert
-            into bulkinsert (id, name,
-            age, height)
-            values (0,
-            'bob' , 21,
-            123 ),(1,
-            'jim' , 56,
-            45 ),(2,
-            'fred' , 100,
-            180 )"""))
-        yield from cursor.execute('commit')
-        yield from self._verify_records(data)
-
-    @run_until_complete
-    def test_bulk_insert_single_record(self):
-        conn = self.connections[0]
-        cursor = conn.cursor()
-        data = [(0, "bob", 21, 123)]
-        yield from cursor.executemany(
-            "insert into bulkinsert (id, name, age, height) "
-            "values (%s,%s,%s,%s)", data)
-        yield from cursor.execute('commit')
-        yield from self._verify_records(data)
---
+++ tests/test_bulk_inserts.py
@@ -0,0 +1,92 @@
+import asyncio
+import aiomysql
+from tests import base
+from tests._testutils import run_until_complete
+
+
+class TestBulkInserts(base.AIOPyMySQLTestCase):
+    cursor_type = aiomysql.cursors.DictCursor
+
+    def setUp(self):
+        super(TestBulkInserts, self).setUp()
+        self.conn = conn = self.connections[0]
+        c = conn.cursor(self.cursor_type)
+
+        @asyncio.coroutine
+        def prepare():
+            # create a table ane some data to query
+            yield from c.execute("drop table if exists bulkinsert;")
+            yield from c.execute(
+                """CREATE TABLE bulkinsert
+                (
+                id int(11),
+                name char(20),
+                age int,
+                height int,
+                PRIMARY KEY (id)
+                )
+                """)
+
+        self.loop.run_until_complete(prepare())
+
+    @asyncio.coroutine
+    def _verify_records(self, data):
+        conn = self.connections[0]
+        cursor = conn.cursor()
+        yield from cursor.execute(
+            "SELECT id, name, age, height from bulkinsert")
+        result = cursor.fetchall()
+        yield from cursor.execute('commit')
+        self.assertEqual(sorted(data), sorted(result))
+
+    @run_until_complete
+    def test_bulk_insert(self):
+        conn = self.connections[0]
+        cursor = conn.cursor()
+
+        data = [(0, "bob", 21, 123), (1, "jim", 56, 45), (2, "fred", 100, 180)]
+        yield from cursor.executemany(
+            "insert into bulkinsert (id, name, age, height) "
+            "values (%s,%s,%s,%s)", data)
+        self.assertEqual(
+            cursor._last_executed, bytearray(
+                b"insert into bulkinsert (id, name, age, height) values "
+                b"(0,'bob',21,123),(1,'jim',56,45),(2,'fred',100,180)"))
+        yield from cursor.execute('commit')
+        yield from self._verify_records(data)
+
+    @run_until_complete
+    def test_bulk_insert_multiline_statement(self):
+        conn = self.connections[0]
+        cursor = conn.cursor()
+        data = [(0, "bob", 21, 123), (1, "jim", 56, 45), (2, "fred", 100, 180)]
+        yield from cursor.executemany("""insert
+            into bulkinsert (id, name,
+            age, height)
+            values (%s,
+            %s , %s,
+            %s )
+             """, data)
+        self.assertEqual(cursor._last_executed, bytearray(b"""insert
+            into bulkinsert (id, name,
+            age, height)
+            values (0,
+            'bob' , 21,
+            123 ),(1,
+            'jim' , 56,
+            45 ),(2,
+            'fred' , 100,
+            180 )"""))
+        yield from cursor.execute('commit')
+        yield from self._verify_records(data)
+
+    @run_until_complete
+    def test_bulk_insert_single_record(self):
+        conn = self.connections[0]
+        cursor = conn.cursor()
+        data = [(0, "bob", 21, 123)]
+        yield from cursor.executemany(
+            "insert into bulkinsert (id, name, age, height) "
+            "values (%s,%s,%s,%s)", data)
+        yield from cursor.execute('commit')
+        yield from self._verify_records(data)
