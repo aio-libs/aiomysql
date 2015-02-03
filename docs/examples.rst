7c7fa57940c84a983baf701eeef13a11acaedcc9
Nickolai Novik <is.infinity@yahoo.com>
2015-02-04 00:07:18 +0200

    add sa example

---
+++ docs/examples.rst
@@ -0,0 +1,22 @@
+Examples of aiomysql usage
+==========================
+
+Below is a list of examples from `aiomysql/examples
+<https://github.com/aio-libs/aiomysql/tree/master/examples>`_
+
+Every example is a correct tiny python program.
+
+.. _aiomysql-examples-simple:
+
+Low-level API
+-------------
+
+.. literalinclude:: ../examples/example.py
+
+
+.. _aiomysql-examples-sa-simple:
+
+Simple sqlalchemy usage
+-----------------------
+
+.. literalinclude:: ../examples/example_sa.py
---
+++ examples/example_sa.py
@@ -0,0 +1,29 @@
+import asyncio
+import sqlalchemy as sa
+
+from aiomysql.sa import create_engine
+
+
+metadata = sa.MetaData()
+
+tbl = sa.Table('tbl', metadata,
+   sa.Column('id', sa.Integer, primary_key=True),
+   sa.Column('val', sa.String(255)))
+
+
+@asyncio.coroutine
+def go():
+   engine = yield from create_engine(user='root',
+                                     db='aiomysql',
+                                     host='127.0.0.1',
+                                     password='')
+
+   with (yield from engine) as conn:
+       yield from conn.execute(tbl.insert().values(val='abc'))
+
+       res = yield from conn.execute(tbl.select())
+       for row in res:
+           print(row.id, row.val)
+
+
+asyncio.get_event_loop().run_until_complete(go())
