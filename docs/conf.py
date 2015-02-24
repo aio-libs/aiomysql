b7c2c0b7480b9d93533338e67f5511963ab5d63a
Nickolai Novik <is.infinity@yahoo.com>
2015-02-24 23:20:03 +0200

    fix sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -294,7 +294,3 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
-
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
