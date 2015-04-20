3c85fb63633e639a3d5d23f17747b54978a82bde
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-04-20 20:10:26 +0300

    Add lightweight test for async with for cursor

--- tests/test_async_with.py
+++ tests/test_async_with.py
@@ -45,3 +45,18 @@ class TestAsyncWith(AIOPyMySQLTestCase):
             self.assertTrue(cur.closed)
 
         self.loop.run_until_complete(go())
+
+    def test_cursor_lightweight(self):
+
+        async def go():
+            conn = self.connections[0]
+            cur = await conn.cursor()
+            await cur.execute('SELECT * from tbl;')
+
+            self.assertFalse(cur.closed)
+            async with cur:
+                pass
+
+            self.assertTrue(cur.closed)
+
+        self.loop.run_until_complete(go())
