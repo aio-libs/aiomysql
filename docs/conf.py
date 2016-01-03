a9c029a7e628b9751339735f5f7f1db10863f854
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-01-03 21:45:58 +0200

    Update conf.py
    Extend to 2016 year

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'aiomysql'
-copyright = '2015, Nikolay Novik'
+copyright = '2015,2016 Nikolay Novik'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
