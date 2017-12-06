a429240c2e2ff430457c8d3aa6fe75de647c6701
Nikolay Novik <nickolainovik@gmail.com>
2017-12-06 19:55:50 +0200

    try to fix formatting

--- aiomysql/__init__.py
+++ aiomysql/__init__.py
@@ -33,7 +33,7 @@ from .connection import Connection, connect
 from .cursors import Cursor, SSCursor, DictCursor, SSDictCursor
 from .pool import create_pool, Pool
 
-__version__ = '0.0.10'
+__version__ = '0.0.11'
 
 __all__ = [
 
