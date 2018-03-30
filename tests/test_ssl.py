69937db74e122236485b573c8ce49b497cefb3b3
Terry Cain <terry@terrys-home.co.uk>
2018-03-30 01:25:03 +0100

    Added docker testcase for TLS

--- tests/test_ssl.py
+++ tests/test_ssl.py
@@ -1,25 +1,26 @@
-# import asyncio
-# import ssl
-#
-# from aiomysql import create_pool
-#
-#
-# async def main():
-#     ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-#     ctx.check_hostname = False
-#     ctx.verify_mode = ssl.CERT_NONE
-#
-#     # mysql_clear_password for connecting to rds
-#     async with create_pool(host='127.0.0.1', port=3308, user='root',
-#                            password='test1234',
-#                            db='testdb', loop=loop, ssl=ctx,
-#                            auth_plugin='mysql_clear_password') as pool:
-#         async with pool.get() as conn:
-#             async with conn.cursor() as cur:
-#                 await cur.execute("SHOW TABLES;")
-#                 value = await cur.fetchone()
-#                 print(value)
-#
-#
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(main())
+from aiomysql import create_pool
+
+import pytest
+
+
+@pytest.mark.run_loop
+async def test_tls_connect(mysql_server, loop):
+
+    async with create_pool(**mysql_server['conn_params'], loop=loop) as pool:
+        async with pool.get() as conn:
+            async with conn.cursor() as cur:
+                # Run simple command
+                await cur.execute("SHOW DATABASES;")
+                value = await cur.fetchall()
+
+                values = [item[0] for item in value]
+                # Spot check the answers, we should at least have mysql and information_schema
+                assert 'mysql' in values, 'Could not find the "mysql" table'
+                assert 'information_schema' in values, 'Could not find the "mysql" table'
+
+                # Check TLS variables
+                await cur.execute("SHOW STATUS LIKE '%Ssl_version%';")
+                value = await cur.fetchone()
+
+                # The context has TLS
+                assert value[1].startswith('TLS'), 'Not connected to the database with TLS'
