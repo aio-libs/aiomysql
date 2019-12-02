31ecda69c3c2b2aed9f66473c0ae209360837384
Andrey Egorov <andr06@gmail.com>
2019-12-02 16:10:40 +0300

    Catching connection lost on _write_bytes

--- aiomysql/connection.py
+++ aiomysql/connection.py
@@ -514,10 +514,7 @@ class Connection:
             if self.autocommit_mode is not None:
                 await self.autocommit(self.autocommit_mode)
         except Exception as e:
-            if self._writer:
-                self._writer.transport.close()
-            self._reader = None
-            self._writer = None
+            self.close()
             raise OperationalError(2003,
                                    "Can't connect to MySQL server on %r" %
                                    self._host) from e
@@ -605,7 +602,12 @@ class Connection:
         return data
 
     def _write_bytes(self, data):
-        return self._writer.write(data)
+        try:
+            return self._writer.write(data)
+        except RuntimeError as e:
+            self.close()
+            msg = "Lost connection to MySQL server during query (%s)" % (e,)
+            raise OperationalError(2013, msg) from e
 
     async def _read_query_result(self, unbuffered=False):
         self._result = None
