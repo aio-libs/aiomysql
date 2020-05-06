e3c1bb808d0af308e21d2488be75a40dfd054b78
Nan GUO <guonanhq@gmail.com>
2020-05-06 19:56:12 +0200

    chore(flake8): fixed flake8 errors (#484)

--- examples/example_oldstyle.py
+++ examples/example_oldstyle.py
@@ -7,9 +7,14 @@ loop = asyncio.get_event_loop()
 
 @asyncio.coroutine
 def test_example():
-    conn = yield from aiomysql.connect(host='127.0.0.1', port=3306,
-                                       user='root', password='', db='mysql',
-                                       loop=loop)
+    conn = yield from aiomysql.connect(
+        host='127.0.0.1',
+        port=3306,
+        user='root',
+        password='',
+        db='mysql',
+        loop=loop
+    )
 
     cur = yield from conn.cursor()
     yield from cur.execute("SELECT Host,User FROM user")
