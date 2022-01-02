fb2e59eafda1f580326adc174069c4912961f5d1
Pedro Américo <pedroamericolouzada@uol.com.br>
2022-01-02 07:25:57 -0300

    add support to pymysql 1.0.2

--- aiomysql/connection.py
+++ aiomysql/connection.py
@@ -16,7 +16,6 @@ from pymysql.constants import SERVER_STATUS
 from pymysql.constants import CLIENT
 from pymysql.constants import COMMAND
 from pymysql.constants import FIELD_TYPE
-from pymysql.util import byte2int, int2byte
 from pymysql.converters import (escape_item, encoders, decoders,
                                 escape_string, escape_bytes_prefixed, through)
 from pymysql.err import (Warning, Error,
@@ -28,14 +27,14 @@ from pymysql.err import (Warning, Error,
 from pymysql.connections import TEXT_TYPES, MAX_PACKET_LEN, DEFAULT_CHARSET
 from pymysql.connections import _auth
 
-from pymysql.connections import pack_int24
+from pymysql.connections import _pack_int24
 
 from pymysql.connections import MysqlPacket
 from pymysql.connections import FieldDescriptorPacket
 from pymysql.connections import EOFPacketWrapper
 from pymysql.connections import OKPacketWrapper
 from pymysql.connections import LoadLocalPacketWrapper
-from pymysql.connections import lenenc_int
+from pymysql.connections import _lenenc_int
 
 # from aiomysql.utils import _convert_to_str
 from .cursors import Cursor
@@ -304,7 +303,7 @@ class Connection:
         if self._writer is None:
             # connection has been closed
             return
-        send_data = struct.pack('<i', 1) + int2byte(COMMAND.COM_QUIT)
+        send_data = struct.pack('<i', 1) + bytes([COMMAND.COM_QUIT])
         self._writer.write(send_data)
         await self._writer.drain()
         self.close()
@@ -545,7 +544,7 @@ class Connection:
         """
         # Internal note: when you build packet manually and calls
         # _write_bytes() directly, you should set self._next_seq_id properly.
-        data = pack_int24(len(payload)) + int2byte(self._next_seq_id) + payload
+        data = _pack_int24(len(payload)) + bytes([self._next_seq_id]) + payload
         self._write_bytes(data)
         self._next_seq_id = (self._next_seq_id + 1) % 256
 
@@ -747,7 +746,7 @@ class Connection:
             authresp = self._password.encode('latin1') + b'\0'
 
         if self.server_capabilities & CLIENT.PLUGIN_AUTH_LENENC_CLIENT_DATA:
-            data += lenenc_int(len(authresp)) + authresp
+            data += _lenenc_int(len(authresp)) + authresp
         elif self.server_capabilities & CLIENT.SECURE_CONNECTION:
             data += struct.pack('B', len(authresp)) + authresp
         else:  # pragma: no cover
@@ -987,7 +986,7 @@ class Connection:
         packet = await self._read_packet()
         data = packet.get_all_data()
         # logger.debug(dump_packet(data))
-        self.protocol_version = byte2int(data[i:i + 1])
+        self.protocol_version = data[i]
         i += 1
 
         server_end = data.find(b'\0', i)
