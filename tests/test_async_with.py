3d246107cab392d3455f98d66d912622a0f24a8a
Richard Schwab <gitrichardschwab-7a2qxq42kj@central-intelligence.agency>
2022-07-09 22:16:43 +0200

    Fix use of deprecated SQLAlchemy functionality in tests
    Fixes the following deprecation warning:
    SADeprecationWarning: The FromClause.count() method is deprecated, and will be removed in a future release.   Please use the count function available from the func namespace.
    
    References:
    https://docs.sqlalchemy.org/en/13/core/metadata.html#sqlalchemy.schema.Table.count
    https://docs.sqlalchemy.org/en/13/core/functions.html#sqlalchemy.sql.functions.count

--- tests/test_async_with.py
+++ tests/test_async_with.py
@@ -4,7 +4,7 @@ import aiomysql
 import pytest
 
 from aiomysql import sa, create_pool, DictCursor, Cursor
-from sqlalchemy import MetaData, Table, Column, Integer, String
+from sqlalchemy import MetaData, Table, Column, Integer, String, func, select
 
 
 meta = MetaData()
@@ -165,7 +165,7 @@ async def test_sa_connection(table, mysql_params, loop):
 async def test_sa_transaction(table, mysql_params, loop):
     async with sa.create_engine(loop=loop, **mysql_params) as engine:
         async with engine.acquire() as connection:
-            cnt = await connection.scalar(tbl.count())
+            cnt = await connection.scalar(select([func.count()]).select_from(tbl))
             assert 3 == cnt
 
             async with (await connection.begin()) as tr:
@@ -173,7 +173,7 @@ async def test_sa_transaction(table, mysql_params, loop):
                 await connection.execute(tbl.delete())
 
             assert not tr.is_active
-            cnt = await connection.scalar(tbl.count())
+            cnt = await connection.scalar(select([func.count()]).select_from(tbl))
             assert 0 == cnt
 
 
@@ -181,7 +181,7 @@ async def test_sa_transaction(table, mysql_params, loop):
 async def test_sa_transaction_rollback(loop, mysql_params, table):
     async with sa.create_engine(loop=loop, **mysql_params) as engine:
         async with engine.acquire() as conn:
-            cnt = await conn.scalar(tbl.count())
+            cnt = await conn.scalar(select([func.count()]).select_from(tbl))
             assert 3 == cnt
 
             with pytest.raises(RuntimeError) as ctx:
@@ -191,7 +191,7 @@ async def test_sa_transaction_rollback(loop, mysql_params, table):
                     raise RuntimeError("Exit")
             assert str(ctx.value) == "Exit"
             assert not tr.is_active
-            cnt = await conn.scalar(tbl.count())
+            cnt = await conn.scalar(select([func.count()]).select_from(tbl))
             assert 3 == cnt
 
 
