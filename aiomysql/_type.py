2eef4368234a37945750542f9a9fd9f42f04c73c
Trim21 <trim21.me@gmail.com>
2024-03-23 20:55:45 +0800

    Update _type.py

--- aiomysql/_type.py
+++ aiomysql/_type.py
@@ -10,9 +10,11 @@ class Description(NamedTuple):
     display_size: Optional[int]
     #: the size in bytes of the column associated to this column on the server.
     internal_size: int
-    #: total number of significant digits in columns of type NUMERIC. None for other types.
+    #: total number of significant digits in columns of type NUMERIC.
+    #: None for other types.
     precision: Optional[int]
-    #: count of decimal digits in the fractional part in columns of type NUMERIC. None for other types.
+    #: count of decimal digits in the fractional part in columns of type NUMERIC.
+    #: None for other types.
     scale: Optional[int]
     #: always None as not easy to retrieve from the libpq.
     null_ok: Optional[bool]
